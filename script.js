f1804fd05e5b3ef55eb659133f3a113b9f45fca7
Paulo <124407850+PauloSantos2002@users.noreply.github.com>
2025-10-08 21:26:32 -0300

    finalização do projeto
    feita a lógica do JavaScript, melhorias no css, media query, ajustes no html.

--- script.js
+++ script.js
@@ -1,2 +1,31 @@
-const form = document.querySelectorAll('input')
+const inputs = document.querySelectorAll('input')
+const spans = document.querySelectorAll('span')
+const mensagem = document.getElementById('messageSendId')
 
+function mostrarMensagem() {
+    mensagem.style.display ='block'
+    setTimeout(() => {
+        mensagem.style.display ='none'
+    }, 3000);
+}
+
+function validar() {
+    let valido = true
+    inputs.forEach((v, index) =>{
+        
+        if (v.value ==='') {
+            spans[index].classList.add('erroSpan')
+            v.classList.add('erroInput')
+            valido = false
+        }else{
+            spans[index].classList.remove('erroSpan')
+            v.classList.remove('erroInput')
+        }
+
+    })
+
+    if (valido) {
+        console.log('validado')
+        mostrarMensagem()
+    }
+}
